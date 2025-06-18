62ea4d49aa93245d6ba7b3f2453190c375625edc
IsaiahDNevin <isaiahdnevin@gmail.com>
2025-06-18 16:28:15 -0700

    Add tailwind and made more dynamic

--- my_site/settings.py
+++ my_site/settings.py
@@ -53,11 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tailwind',
-    'theme',
 ]
-
-TAILWIND_APP_NAME = "theme"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
